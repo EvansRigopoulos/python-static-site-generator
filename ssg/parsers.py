e5c603b0f4c70122df5b76f5cc609c0a7bb23f56
CITRITE\evangelosr1 <evangelos.rigopoulos1@citrix.com>
2022-12-15 12:40:33 +0200

    stage 3

---
+++ ssg/parsers.py
@@ -0,0 +1,43 @@
+from typing import List
+from pathlib import Path
+import shutil
+
+class Parser:
+    
+    extensions = []
+    extensions:str
+
+    def valid_extensions(extension):
+        return extension in self.extensions
+
+    def parse(path:Path,source:Path,dest:Path) -> Path:
+        raise NotImplementedError
+
+    def read(path):
+        with open(path) as file:
+            return read(file)
+
+    def write(path,dest,content,ext =".html"):
+        full_path = dest / with_suffix(ext).name
+        with open(full_path) as file:
+            file.write(content)
+
+    def copy(path,source,dest):
+        shutil.copy2(path,dest / source)
+
+
+    class ResourceParser:
+
+        extensions = [".jpeg",".png",".gif",".css",".html"]
+
+        def parse(path:Path,source:Path,dest:Path) -> Path:
+            raise NotImplementedError
+
+        def copy(path,source,dest):
+            super.copy()
+            
+
+    
+
+    
+        
--- ssg/site.py
+++ ssg/site.py
@@ -9,14 +9,28 @@ class Site:
         directory = self.dest / relative_to(self.source)
         mkdir(directory,parents=True,exist_ok=True)
         
-    def build():
+    def build(self):
         mkdir(self.dest,parents=True,exist_ok=True)
         
         for path in self.source.rglob("*"):
             if path :
                 create_dir(path)
+            elif path is file:
+                run_parser(path)
                 
-     
+    def load_parser(self,extension):
+        for parser in self.parsers:
+            if valid_extension(extensions):
+                return parser
+
+    def run_parser(self,path):
+        parser = load_parser(path.suffix)
+
+        if parser is not None:
+            parser.parse(path,self.source,self.dest)
+        else:
+            print("Not Implemented")
+            
 
 
 
