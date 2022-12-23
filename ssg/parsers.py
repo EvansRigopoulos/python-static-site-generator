64f04ad1e8f5c2bd981c94cc6aa819b3dd494548
CITRITE\evangelosr1 <evangelos.rigopoulos1@citrix.com>
2022-12-23 12:35:51 +0200

    Parser subclasses

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,7 +1,10 @@
 from typing import List
 from pathlib import Path
 import shutil
-
+import sys
+from docutils.core import publish_parts
+from markdown import markdown
+from ssg.content import Content
 class Parser:
     
     extensions:List[str] = []
@@ -33,9 +36,15 @@ class ResourceParser(Parser):
     def parse(self, path, source, dest) :
         self.copy(path, source , dest)
         
-    def copy(self, path, source, dest):
-        super.copy()
-        
+    
+class MarkdownParser(Parser):
+    extensions =[".md",".markdown"]
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path)) 
+        html = markdown(content.body)
+        self.write(path, dest, html)   
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)  
 
 
         
